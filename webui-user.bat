75217a95ef416136617e95ab44a735fd59001a32
developerhost <kanonhara0609@gmail.com>
2025-01-07 11:31:58 +0900

    first commit

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate --precision full --no-half --disable-nan-check
 
 call webui.bat
